a19b51dc9f1349227d1df08e6fb23436f5f5f65d
arias <aarias@ariass-MacBook-Pro.local>
2020-10-11 21:54:54 -0700

    adding documentation to the project

--- test/specs/pixelPerfect.js
+++ test/specs/pixelPerfect.js
@@ -2,8 +2,10 @@ const LandingPage = require('../pageobjects/landing.page');
 
 describe('Pixel Perfect testing',  async function(){
     it('should display pixel-perfect on Chrome browser, viewport 1200px and above.', async function(){
+      //Opening the landing page.
       LandingPage.open();
-      
+
+      //Taking Snapshot using percy to validate pixel perfect page on Percy backed.
       await percySnapshot(browser, "first screenshot",{ widths: [1200, 1400, 1800] })
     });
 });
